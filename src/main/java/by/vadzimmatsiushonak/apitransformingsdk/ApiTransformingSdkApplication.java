54d9c829eebe3cc476ca95d72ad59059b946d56f
Vadzim Matsiushonak <vadzimmatsiushonak@gmail.com>
2023-08-05 01:57:17 +0300

    First commit

---
+++ src/main/java/by/vadzimmatsiushonak/apitransformingsdk/ApiTransformingSdkApplication.java
@@ -0,0 +1,13 @@
+package by.vadzimmatsiushonak.apitransformingsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransformingSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransformingSdkApplication.class, args);
+	}
+
+}
